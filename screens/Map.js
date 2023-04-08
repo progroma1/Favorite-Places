41ffc06df8e38e91e3136f83a7e7ceab3ef88668
Roman Tyniv <roman.tyniv@3ss.tv>
2023-04-08 21:44:12 +0300

    map changes

--- screens/Map.js
+++ screens/Map.js
@@ -1,8 +1,12 @@
-import { StyleSheet } from "react-native";
+import { useState, useLayoutEffect, useCallback } from "react";
+import { Alert, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import IconButton from "../components/UI/IconButton";
 
+const Map = ({ navigation }) => {
 
-const Map = () => {
+    const [selectedLocation, setSelectedLocation] = useState();
+
     const region = {
         latitude: 37.78,
         longitude: -122.43,
@@ -10,8 +14,48 @@ const Map = () => {
         longitudeDelta: 0.0421,
     };
 
+    const selectLocationHandler = (event) => {
+        const lat = event.nativeEvent.coordinate.latitude;
+        const lng = event.nativeEvent.coordinate.longitude;
+
+        setSelectedLocation({ lat: lat, lng: lng });
+    };
+
+    const savePickedLocationHandler = useCallback(() => {
+        if (!selectedLocation) {
+            Alert.alert(
+                'No location picked',
+                'You have to pick a location (by tapping on the map) first!'
+            );
+            return;
+        };
+
+        navigation.navigate('AddPlace', {
+            pickedLat: selectedLocation.lat,
+            pickedLng: selectedLocation.lng
+        })
+    }, [navigation, selectedLocation]);
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: ({ tintColor }) => <IconButton icon='save' color={tintColor} size={24} onPress={savePickedLocationHandler} />
+        });
+    }, [navigation, savePickedLocationHandler]);
+
     return (
-        <MapView initialRegion={region} style={styles.map}></MapView>
+        <MapView initialRegion={region} style={styles.map} onPress={selectLocationHandler}>
+            {selectedLocation && (
+                <Marker
+                    title="Picked Location"
+                    coordinate={
+                        {
+                            latitude: selectedLocation.lat,
+                            longitude: selectedLocation.lng
+                        }
+                    }
+                />
+            )}
+        </MapView>
     );
 };
 
